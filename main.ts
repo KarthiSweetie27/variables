6a332312ae5eca82a138a50681ac7934261bc385
KarthiSweetie27 <122539938+KarthiSweetie27@users.noreply.github.com>
2023-01-26 08:46:28 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(0)
+})
+input.onButtonPressed(Button.AB, function () {
+    _0 = 0
+})
+input.onButtonPressed(Button.B, function () {
+    _1 += 1
+})
+let _1 = 0
+let _0 = 0
+let counter = 0
+basic.showNumber(0)
